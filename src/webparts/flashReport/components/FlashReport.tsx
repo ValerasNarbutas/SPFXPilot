4b8ba4847cad1cf404da82293c87bae9b9909371
Valeras Narbutas <Valeras.Narbutas@macaw.nl>
2018-11-07 16:59:00 +0200

    added dropdowm component - not finished

--- src/webparts/flashReport/components/FlashReport.tsx
+++ src/webparts/flashReport/components/FlashReport.tsx
@@ -3,12 +3,16 @@ import styles from './FlashReport.module.scss';
 import { IFlashReportProps } from './IFlashReportProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { DropdownBasicExample } from './StatusDropDown';
 
 export default class FlashReport extends React.Component<IFlashReportProps, {}> {
   public render(): React.ReactElement<IFlashReportProps> {
     return (
       <div className={ styles.flashReport }>
+
         <DefaultButton text="button" /> 
+        <DropdownBasicExample />
+
       </div>
     );
   }
---
+++ src/webparts/flashReport/components/StatusDropDown.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+import { Dropdown, IDropdown, DropdownMenuItemType, IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
+import { BaseComponent, createRef } from 'office-ui-fabric-react/lib/Utilities';
+
+
+export class DropdownBasicExample extends BaseComponent<
+  {},
+  {
+    selectedItem?: { key: string | number | undefined };
+    selectedItems: string[];
+  }
+> {
+  private _basicDropdown = createRef<IDropdown>();
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      selectedItem: undefined,
+      selectedItems: []
+    };
+  }
+
+  public render() {
+    const { selectedItem, selectedItems } = this.state;
+
+    return (
+      <div className="docs-DropdownExample">
+        <Dropdown
+          placeHolder="Select an Option"
+          label="Basic uncontrolled example:"
+          id="Basicdrop1"
+          ariaLabel="Basic dropdown example"
+          options={[
+            { key: 'Header', text: 'Status', itemType: DropdownMenuItemType.Header },
+            { key: 'G', text: 'Green', title: 'Select green if all good' },
+            { key: 'O', text: 'Orange', title: 'Select orange if failing a bit' },
+            { key: 'R', text: 'Red', title: 'ITS gona BLOW!!!!', disabled: false }
+           
+          ]}
+          onFocus={this._log('onFocus called')}
+          onBlur={this._log('onBlur called')}
+         
+        />
+        <PrimaryButton text="Set focus" onClick={this._onSetFocusButtonClicked} />
+        
+      </div>
+    );
+  }
+
+  public changeState = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
+    console.log('here is the things updating...' + item.key + ' ' + item.text + ' ' + item.selected);
+    this.setState({ selectedItem: item });
+  };
+
+  public onChangeMultiSelect = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
+    const updatedSelectedItem = this.state.selectedItems ? this.copyArray(this.state.selectedItems) : [];
+    if (item.selected) {
+      // add the option if it's checked
+      updatedSelectedItem.push(item.key);
+    } else {
+      // remove the option if it's unchecked
+      const currIndex = updatedSelectedItem.indexOf(item.key);
+      if (currIndex > -1) {
+        updatedSelectedItem.splice(currIndex, 1);
+      }
+    }
+    this.setState({
+      selectedItems: updatedSelectedItem
+    });
+  };
+
+  public copyArray = (array: any[]): any[] => {
+    const newArray: any[] = [];
+    for (let i = 0; i < array.length; i++) {
+      newArray[i] = array[i];
+    }
+    return newArray;
+  };
+
+  private _onSetFocusButtonClicked = (): void => {
+    if (this._basicDropdown.current) {
+      this._basicDropdown.current.focus(true);
+    }
+  };
+
+  private _log(str: string): () => void {
+    return (): void => {
+      console.log(str);
+    };
+  }
+}
